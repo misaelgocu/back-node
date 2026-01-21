dbbc1ed414a24738092eb99b346615a71637f7e0
misaelgocu <misaelgomez00000@gmail.com>
2026-01-21 16:43:38 -0600

    separacion de responsabilidades

---
+++ routes/products.router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { faker }= require('@faker-js/faker');
+
+const router = express.Router();
+
+
+router.get('/', (req, res) =>{
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for(let i =0; i < limit; i++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.avatar(),
+    });
+  }
+  res.json(products);
+});
+
+module.exports = router;
