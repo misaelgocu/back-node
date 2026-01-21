6369638c6c0f1983769d91e896dfe40e45d91741
misaelgocu <misaelgomez00000@gmail.com>
2026-01-21 16:14:25 -0600

    faker para data dinamica

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { faker }= require('@faker-js/faker');
+
 const app = express();
 const port = 3000;
 
@@ -8,12 +10,34 @@ app.get('/', (req, res) =>{
 });
 
 app.get('/nuevo-endpoint', (req, res) => {
-  res.json({
+  res.json([
+    {
     id: 1,
     nombre: 'Producto de ejemplo',
     precio: 25.99,
     disponible: true
-  });
+  },
+  {
+    id: 2,
+    nombre: 'Producto de ejemplo 2',
+    precio: 25.99,
+    disponible: true
+  }
+  ]);
+});
+
+app.get('/products', (req, res) =>{
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for(let i =0; i < limit; i++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.avatar(),
+    });
+  }
+  res.json(products);
 });
 
 app.listen(port, () =>{
