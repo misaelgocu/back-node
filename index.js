0c2d3bf598de60baa99c11c35503f153a5356f3a
misaelgocu <misaelgomez00000@gmail.com>
2026-01-21 15:15:40 -0600

    rutas

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.get('/', (req, res) =>{
   res.send('Hola mi server en express');
 });
 
+app.get('/nuevo-endpoint', (req, res) => {
+  res.json({
+    id: 1,
+    nombre: 'Producto de ejemplo',
+    precio: 25.99,
+    disponible: true
+  });
+});
+
 app.listen(port, () =>{
   console.log('Mi port: ' + port);
 });
