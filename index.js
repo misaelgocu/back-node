d3a8e032a41d19e6e2b4d3e838a9d5ee40af4ec0
misaelgocu <misaelgomez00000@gmail.com>
2026-01-22 15:58:39 -0600

    CORS

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
-
-const { logErrors, errorHandler, boomErrorHandler  } = require('./middlewares/error.handler');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
+
 app.use(express.json());
 
+// 1. Definimos la lista de dominios permitidos (Whitelist)
+const whitelist = [
+  'http://localhost:8080',
+  'http://localhost:3000',
+  'https://mi-tienda-online.com'
+];
 
-app.get('/', (req, res) =>{
-  res.send('Hola mi server en express');
+// 2. Configuración de opciones de CORS
+const options = {
+  origin: (origin, callback) => {
+    // Permitimos peticiones sin origen (como Postman o el mismo servidor)
+    // o si el origen está en la whitelist
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Acceso denegado por políticas de CORS'));
+    }
+  }
+};
+
+// 3. Aplicamos el middleware con las opciones configuradas
+app.use(cors(options));
+
+app.get('/', (req, res) => {
+  res.send('Hola, mi servidor en Express está protegido con CORS');
 });
-
 
 routerApi(app);
 
+// Middlewares de error (Siempre después de las rutas)
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-app.listen(port, () =>{
-  console.log('Mi port: ' + port);
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
-
-
